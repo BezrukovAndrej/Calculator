deb325d1d2393e7bcb51104abd40197055c58a61
Bezrukovandrej <bezrukovandrej@gmail.com>
2023-04-19 09:49:50 +0300

    Initial Commit

---
+++ Сalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Сalculator
+//
+//  Created by Andrey Bezrukov on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
